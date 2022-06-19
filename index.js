29529d7515895b967a7d9a0c79a3c80dff23b5c7
Glenn Nguyo <glennnguyo1000@gmail.com>
2022-06-19 23:49:14 +0300

    Complete

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const values = word.split('')
+  const reversevalues = values.reverse()
+  const reverseString = values.join('')
+  if(word == reverseString){
+    return(true)
+  }else{
+    return(false)
+  }
 }
 
 /* 
@@ -8,6 +16,8 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
+  The values variable is to store the word split and render the information to the
+  second variable which reverses it which then renders its values to be joined and finally it returns true or false
 */
 
 // You can run `node index.js` to view these console logs
